d3904b6b18ac2328df9c0a71f634e8b9a99cbed4
nurgeldiserikbay <nurikserikbay1995@gmail.com>
2025-05-15 00:00:17 +0500

    save

---
+++ docs/assets/web-B_ponA-b.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-BX7qSj5C.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
--- docs/assets/web-Nb4VVtCk.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-BqiFc0S6.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
